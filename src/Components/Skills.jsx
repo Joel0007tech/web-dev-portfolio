5c0c45cfd5527a04e543db3d1d41d2b26c4b5ecd
Joel <ajiolajoel@gmail.com>
2023-05-17 08:11:59 +0100

    update to my portfolio

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,8 +1,132 @@
-import React from 'react'
+import React,{useState} from 'react'
+import image from "../assets/FB_IMG_1632361671640__2_-removebg-preview.png"
+import { Link} from "react-router-dom"
+import {navTexts } from '../constants'
+import {FaUserAlt} from "react-icons/fa"
+import ace from "../assets/ace project.png"
+import hoobank from "../assets/hoobank project.png"
+import architecture from "../assets/architecture project.png"
+import counter from "../assets/counter app.png";
+import ctmaid from "../assets/ctmaid project.png"
 
 const Skills = () => {
+  const [open, setOpen] = useState(false);
+ 
+  const Font ={
+    fontFamily: 'Roboto, sans-serif'
+  }
   return (
-    <div>Skills</div>
+    <div>
+<div
+        className="pt-6 h-full w-full sm:h-full sm:w-full sm:overflow-hidden
+         bg-blue-700 border-b-0 border-r-0 border-l-0 border-t-2 rounded-t-xl"
+      
+      >
+        <div
+          className=" p-6 overflow-hidden
+            sm:border-b-2 sm:border-white sm:rounded-b-md"
+        >
+          <div className="sm:pb-3 sm:pt-3 sm:flex">
+            <img
+              src={image}
+              alt=""
+              className=" fixed sm:fixed sm:top-8 sm:right-24 w-[90px] h-[50px] top-9"
+            />
+            <FaUserAlt
+              className="cursor-pointer sm:cursor-pointer text-white sm:text-white
+              sm:text-2xl text-4xl fixed sm:fixed top-10 sm:right-10 right-60 font-normal sm:top-11 hover:text-gray"
+              onClick={() => setOpen(!open)}
+            />
+          </div>
+        </div>
+        {open && (
+          <div
+            className="overflow-hidden
+          sm:border-b-2 sm:border-white sm:rounded-b-md"
+          >
+            <ul
+              className="flex justify-center flex-col gap-6 sm:gap-2 text-center cursor-pointer
+            pb-10" style={Font}
+            >
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold hover:text-gray-500">
+                <Link to="/" target='_blank'>{navTexts.first}</Link>
+              </li>
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold hover:text-gray-500">
+                <Link to="/about" target='__blank'>{navTexts.second}</Link>
+              </li>
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold hover:text-gray-500">
+                <Link to="/skills" target='_blank'>{navTexts.third}</Link>
+              </li>
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold hover:text-gray-500">
+                <Link to="/contact" target='_blank'>{navTexts.fourth}</Link>
+              </li>   
+            </ul>
+          </div>
+        )}
+        </div>
+        <div className='bg-slate-200'>
+        <h3 className="text-blue-700 font-bold text-xl pt-4" style={Font}>
+          Take A Glance At My Past Projects;
+        </h3>
+        </div>
+        <div className="flex flex-col p-20 gap-14 items-center ">
+          <div className="relative w-[600px] h-[450px] right-40
+           transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-black hover:opacity-50">
+            <img
+        className="w-[600px] h-[450px] hover:shadow-sm"
+        src={hoobank}
+        alt="placeholder"
+      />
+      <div className="absolute top-40 flex gap-4 w-full h-full left-7
+       items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+     <div className="flex flex-col">
+    <h3 className="text-white text-xl" style={Font}>Bulldog Billionaires</h3>
+    <p className="text-white text-sm leading-normal">Bulldog Billionaires is a collection of 2,000 <br/>
+    randomely generated NFTs that exist on <br/>
+    the Solana Blockchain.</p>
+     </div>
+     <a
+              href=""
+              className="text-center bg-purple-600 py-3 px-9 sm:py-2 sm:my-4
+              hover hover:bg-white hover:text-purple-800 text-white font-semibold sm:text-sm
+             w-[170px] sm:w-[250px] h-[50px] sm:h-[40px] m-auto rounded-lg"
+              style={Font}
+            >
+               visit website
+            </a>
+      </div>
+    </div>
+    <div className="relative w-[600px] h-[450px] left-40 
+    transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110  hover:bg-black hover:opacity-50
+    ">
+      <div>
+      <img
+        className="w-[600px] h-[450px]"
+        src={ace}
+        alt="placeholder"
+      />
+    </div>
+      <div className="absolute top-40 gap-4 w-full h-full left-7 flex
+       items-center justify-center opacity-0 hover:opacity-50 transition-opacity duration-300">
+       <div className="flex flex-col bg-black">
+    <h3 className="text-xl text-white" style={Font}>T-connect</h3>
+    <p className="text-sm leading-normal text-white">T-connect is an NFT marketplace platform <br/>
+    where users can lend and rent NFTs</p>
+     </div>
+     <a
+              href=""
+              className="text-center bg-purple-900 py-3 px-9 sm:py-2 sm:my-4
+              hover hover:bg-white hover:text-purple-800 text-white font-semibold sm:text-sm
+             w-[170px] sm:w-[250px] h-[50px] sm:h-[40px] m-auto rounded-lg"
+              style={Font}
+            >
+               visit website
+            </a>
+    
+      </div>
+    </div>
+    </div>
+    </div>
   )
 }
 
