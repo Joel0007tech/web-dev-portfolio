2c10b00c29f94f0c5167b7467f146f33088aec26
Joel <ajiolajoel@gmail.com>
2023-09-08 10:03:59 +0100

    update to my portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import image from "../assets/FB_IMG_1632361671640__2_-removebg-preview.png";
-import { link, navTexts} from "../constants";
-import { FaUserAlt,FaHtml5,FaReact,FaCss3Alt, } from "react-icons/fa";
-import { DiJavascript1} from "react-icons/di";
-import {SiTailwindcss} from "react-icons/si";
+import { link, navTexts } from "../constants";
+import { FaUserAlt, FaHtml5, FaReact, FaCss3Alt } from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import { SiTailwindcss } from "react-icons/si";
 import biography from "../assets/biography.jpg.jpg";
 import blog from "../assets/blog.jpg.jpg";
-import eCommerce from "../assets/e-commerce.jpg.jpg"
+import eCommerce from "../assets/e-commerce.jpg.jpg";
 import branding from "../assets/branding.jpg.jpg";
-import charity from "../assets/charity.jpg.jpg"
+import charity from "../assets/charity.jpg.jpg";
 
 const About = () => {
   const [open, setOpen] = useState(false);
@@ -24,14 +24,13 @@ const About = () => {
          bg-blue-700 border-b-0 border-r-0 border-l-0
           border-t-2 rounded-t-xl m-auto sm:mt-0"
       >
-        <div
-          className=" overflow-hidden"
-        >
+        <div className=" overflow-hidden">
           <div className="sm:flex flex justify-between">
             <img
               src={image}
               alt=""
-              className="w-[90px] sm:w-[50px] h-[50px] sm:h-[30px]"/>
+              className="w-[90px] sm:w-[50px] h-[50px] sm:h-[30px]"
+            />
             <FaUserAlt
               className="cursor-pointer sm:cursor-pointer text-white sm:text-white
               sm:text-2xl text-4xl float-right font-normal hover:text-gray sm:float-right"
@@ -46,24 +45,36 @@ const About = () => {
           >
             <ul
               className="flex justify-center flex-col gap-6 sm:gap-4 text-center cursor-pointer
-            pb-10" style={Font} data-aos="fade-right" data-aos-delay="100" data-aos-duration="1000"
+            pb-10"
+              style={Font}
+              data-aos="fade-right"
+              data-aos-delay="100"
+              data-aos-duration="1000"
             >
               <li className="text-white font-bold text-2xl sm:text-sm sm:font-semibold hover:text-gray-500">
-                <Link to="/" target='_blank'>{navTexts.first}</Link>
+                <Link to="/" target="_blank">
+                  {navTexts.first}
+                </Link>
               </li>
               <li className="text-white font-bold text-2xl sm:text-sm sm:font-semibold hover:text-gray-500">
-                <Link to="/about" target='__blank'>{navTexts.second}</Link>
+                <Link to="/about" target="__blank">
+                  {navTexts.second}
+                </Link>
               </li>
               <li className="text-white font-bold text-2xl sm:text-sm sm:font-semibold hover:text-gray-500">
-                <Link to="/skills" target='_blank'>{navTexts.third}</Link>
+                <Link to="/skills" target="_blank">
+                  {navTexts.third}
+                </Link>
               </li>
               <li className="text-white font-bold text-2xl sm:text-sm sm:font-semibold hover:text-gray-500">
-                <Link to="/contact" target='_blank'>{navTexts.fourth}</Link>
-              </li>   
+                <Link to="/contact" target="_blank">
+                  {navTexts.fourth}
+                </Link>
+              </li>
             </ul>
           </div>
         )}
-        </div>
+      </div>
       <div className=" bg-slate-200 h-full w-full m-auto sm:h-full sm:w-full sm:overflow-hidden">
         <h3
           className="text-blue-700 font-bold text-center text-4xl pt-5 sm:text-2xl"
@@ -85,71 +96,118 @@ const About = () => {
                sm:left-0 sm:text-lg sm:pt-0 sm:text-center sm:bottom-10 2xl:text-center "
               style={Font}
             >
-              JOEL is a web developer, with over 3 years<br/>
-              of eperience, in developing websites and <br className="2xl:block sm:hidden"/>
-               webApps, for individuals, and companies, <br className="2xl:block sm:hidden"/>
-              helping them achieve their aims and objectives.<br/>
-              I have experience in:<br/>
+              JOEL is a web developer, with over 3 years
+              <br />
+              of eperience, in developing websites and{" "}
+              <br className="2xl:block sm:hidden" />
+              webApps, for individuals, and companies,{" "}
+              <br className="2xl:block sm:hidden" />
+              helping them achieve their aims and objectives.
+              <br />
+              I have experience in:
+              <br />
               <span className="text-gray-500 font-bold">
                 {" "}
                 HTML, CSS, JAVASCRIPT, TAILWINDCSS, AND REACTjs.
               </span>
             </p>
             <div className="flex relative right-24 gap-3 sm:bottom-20 sm:justify-center sm:left-1">
-              <FaHtml5 className="w-[100px] h-[200px] fill-red-600 sm:w-[50px]"/>
-              <FaCss3Alt className="w-[100px] h-[200px] fill-blue-500 sm:w-[50px]"/>
-              <DiJavascript1 className="w-[100px] h-[200px] sm:w-[50px]"/>
-              <SiTailwindcss className="w-[100px] h-[200px] text-blue-400 sm:w-[50px]"/>
-              <FaReact className="w-[100px] h-[200px] text-blue-400 sm:w-[50px]"/>
+              <FaHtml5 className="w-[100px] h-[200px] fill-red-600 sm:w-[50px]" />
+              <FaCss3Alt className="w-[100px] h-[200px] fill-blue-500 sm:w-[50px]" />
+              <DiJavascript1 className="w-[100px] h-[200px] sm:w-[50px]" />
+              <SiTailwindcss className="w-[100px] h-[200px] text-blue-400 sm:w-[50px]" />
+              <FaReact className="w-[100px] h-[200px] text-blue-400 sm:w-[50px]" />
             </div>
             <div>
-              <a
-                href=""
-                className=" bg-blue-700 border rounded text-yellow-500 font-bold sm:font-medium
-      hover:text-blue-700 hover:bg-slate-400 pr-28 sm:pr-5 pl-28 sm:pl-5 pt-5
-       sm:pt-2 pb-5 sm:pb-2 text-2xl sm:text-lg "
-                style={Font}
-              >
-                {link.secondText}
-              </a>
+             <a href="http://" className="w-full bg-blue-700 text-white rounded-md px-10 py-3 2xl:relative 2xl:right-20 leading-normal">{link.secondText}</a> 
             </div>
           </div>
         </div>
         <div className="sm:relative sm:bottom-36 pt-20">
-        <h3 className="text-blue-700 font-bold text-center text-3xl sm:text-xl" style={Font}>
-          Few Out Of The Categories Of Websites(apps) I Will Develop For You;</h3>
-        <div className="flex gap-4 p-6 sm:flex-col" data-aos="zoom-out" data-aos-delay="100" data-aos-duration="1000">
-         <div>
-          <img src={biography} alt="biography" className="w-[350px] h-[180px]"/>
-          <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
-            <p className="text-blue-700 font-bold text-center text-lg" style={Font}>Biography</p>
+          <h3
+            className="text-blue-700 font-bold text-center text-3xl sm:text-xl"
+            style={Font}
+          >
+            Few Out Of The Categories Of Websites(apps) I Will Develop For You;
+          </h3>
+          <div
+            className="flex gap-4 p-6 sm:flex-col"
+            data-aos="zoom-out"
+            data-aos-delay="100"
+            data-aos-duration="1000"
+          >
+            <div>
+              <img
+                src={biography}
+                alt="biography"
+                className="w-[350px] h-[180px]"
+              />
+              <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
+                <p
+                  className="text-blue-700 font-bold text-center text-lg"
+                  style={Font}
+                >
+                  Biography
+                </p>
+              </div>
+            </div>
+            <div>
+              <img src={blog} alt="blog" className="w-[350px] h-[180px]" />
+              <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
+                <p
+                  className="text-blue-700 font-bold text-center text-lg"
+                  style={Font}
+                >
+                  Blog
+                </p>
+              </div>
+            </div>
+            <div>
+              <img
+                src={eCommerce}
+                alt="e-commerce"
+                className="w-[350px] h-[180px]"
+              />
+              <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
+                <p
+                  className="text-blue-700 font-bold text-center text-lg"
+                  style={Font}
+                >
+                  E-commerce
+                </p>
+              </div>
+            </div>
+            <div>
+              <img
+                src={branding}
+                alt="branding"
+                className="w-[350px] h-[180px]"
+              />
+              <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
+                <p
+                  className="text-blue-700 font-bold text-center text-lg"
+                  style={Font}
+                >
+                  Branding
+                </p>
+              </div>
+            </div>
+            <div>
+              <img
+                src={charity}
+                alt="charity"
+                className="w-[350px] h-[180px]"
+              />
+              <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
+                <p
+                  className="text-blue-700 font-bold text-center text-lg"
+                  style={Font}
+                >
+                  Charity
+                </p>
+              </div>
+            </div>
           </div>
-         </div>
-         <div>
-          <img src={blog} alt="blog" className="w-[350px] h-[180px]"/>
-          <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
-            <p className="text-blue-700 font-bold text-center text-lg" style={Font}>Blog</p>
-          </div>
-         </div>
-         <div>
-          <img src={eCommerce} alt="e-commerce" className="w-[350px] h-[180px]"/>
-          <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
-            <p className="text-blue-700 font-bold text-center text-lg" style={Font}>E-commerce</p>
-          </div>
-         </div>
-         <div>
-          <img src={branding} alt="branding" className="w-[350px] h-[180px]"/>
-          <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
-            <p className="text-blue-700 font-bold text-center text-lg" style={Font}>Branding</p>
-          </div>
-         </div>
-         <div>
-          <img src={charity} alt="charity" className="w-[350px] h-[180px]"/>
-          <div className="bg-white w-[150px] relative bottom-28 m-auto pt-2 pb-2">
-            <p className="text-blue-700 font-bold text-center text-lg" style={Font}>Charity</p>
-          </div>
-         </div>
-        </div>
         </div>
       </div>
     </div>
